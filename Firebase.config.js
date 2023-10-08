69ca6595499d73a35abce694a7127a4b70b24e88
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2023-10-09 03:50:19 +0600

    custom package  section add

---
+++ Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB76KNf7S7Yls1rUyv00x7Gr-JQC4ijGDo",
+  authDomain: "fateforever-4c243.firebaseapp.com",
+  projectId: "fateforever-4c243",
+  storageBucket: "fateforever-4c243.appspot.com",
+  messagingSenderId: "660822721121",
+  appId: "1:660822721121:web:7700190a5639f763586020"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth()
--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 const Cart = ({ cardItem }) => {
 
+  console.log(cardItem)
     let sum = 0;
 
 
     cardItem.map(result=>{
-        const total = parseFloat(result.price)
-        console.log(total)
-    })
+        const priceInt = parseInt(result.price)
+        sum+=priceInt
+      })
+      // console.log(sum)
    
 
   return (
     <div className="bg-white h-fit">
-      <h2 className="text-lg px-8 py-4 mb-11 bg-rose-700  text-center text-white">
+      <h2 className="text-lg px-8 py-4 bg-rose-700  text-center text-white">
         Create your own packages
       </h2>
 
-      <ul className="px-8 ">
+      <div className="px-8 py-8">
+      <ul className="">
         {cardItem.map((result,index) =>(
            <div className="flex gap-4">
             <span>{index+1}.</span>
@@ -28,8 +31,15 @@ const Cart = ({ cardItem }) => {
 
       </ul>
       <div>
+      <div className="">
       <hr className="my-3"/>
-      <p>{sum}</p>
+      <div className="flex gap-4 justify-between">
+      <p>Total: </p>
+      <span>{sum}</span>
+      </div>
+      <button className="btn btn-primary w-full mt-5 text-white bg-rose-700 border-0 hover:bg-black hover:text-white">Purchase</button>
+      </div>
+      </div>
       </div>
     </div>
   );
